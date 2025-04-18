ef395aad8389dddf3a4f8b3b37b89470a562f750
Marcos Cláudio do Nascimento Matos <91806717+marcosarmitag@users.noreply.github.com>
2025-04-18 13:53:24 -0300

    Ajustes

--- contentScript.js
+++ contentScript.js
@@ -1,11 +1,11 @@
 // Escuta mensagens do background script
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "fillInput") {
-      const activeElement = document.activeElement;
-  
-      // Verifica se o elemento ativo é um input ou textarea
-      if (activeElement && (activeElement.tagName === "INPUT" || activeElement.tagName === "TEXTAREA")) {
-        activeElement.value = request.value; // Preenche o campo com o valor gerado
-      }
+  if (request.action === "fillInput") {
+    const activeElement = document.activeElement;
+
+    // Verifica se o elemento ativo é um input ou textarea
+    if (activeElement && (activeElement.tagName === "INPUT" || activeElement.tagName === "TEXTAREA")) {
+      activeElement.value = request.value; // Preenche o campo com o valor gerado
     }
-  });
+  }
+});
--- engrenagens.js
+++ engrenagens.js
@@ -1,9 +1,11 @@
-// Função para verificar se a formatação está habilitada
 function isFormattingEnabled() {
   return document.getElementById("formatOption").checked;
 }
 
-// Função para definir um cookie
+function isAutoCopyEnabled() {
+  return document.getElementById("autoCopyOption").checked;
+}
+
 function setCookie(name, value, days) {
   const date = new Date();
   date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
@@ -11,7 +13,6 @@ function setCookie(name, value, days) {
   document.cookie = name + "=" + value + ";" + expires + ";path=/";
 }
 
-// Função para obter um cookie
 function getCookie(name) {
   const nameEQ = name + "=";
   const cookies = document.cookie.split(';');
@@ -22,26 +23,38 @@ function getCookie(name) {
   return null;
 }
 
-// Função para salvar a opção de formatação usando cookie e localmente
 function saveFormatOption() {
   const isFormatted = isFormattingEnabled();
   setCookie("formatOption", isFormatted, 365);
   carregarHistorico();
 }
 
-// Função para carregar a opção de formatação salva no cookie
 function loadFormatOption() {
   const formatOption = getCookie("formatOption");
 
   if (formatOption !== null) {
     document.getElementById("formatOption").checked = (formatOption === 'true');
-    console.log("Opção de formatação carregada:", formatOption);
   } else {
-    console.log("Nenhuma opção de formatação salva encontrada. Usando padrão (desmarcado).");
+    console.log("Nenhuma opção de formatação salva encontrada.");
   }
 }
 
-// Função para copiar texto para a área de transferência e mostrar uma notificação
+function saveAutoCopyOption() {
+  const isAutoCopy = isAutoCopyEnabled();
+  setCookie("autoCopyOption", isAutoCopy, 365);
+  carregarHistorico();
+}
+
+function loadAutoCopyOption() {
+  const autoCopyOption = getCookie("autoCopyOption");
+
+  if (autoCopyOption !== null) {
+    document.getElementById("autoCopyOption").checked = (autoCopyOption === 'true');
+  } else {
+    console.log("Nenhuma opção salva encontrada.");
+  }
+}
+
 function copyToClipboard(text) {
   navigator.clipboard.writeText(text).then(() => {
     //alert("Valor copiado para a área de transferência!");
@@ -50,7 +63,6 @@ function copyToClipboard(text) {
   });
 }
 
-// Função para gerar CPF válido com opção de formatação e copiar para área de transferência
 function generateCPF() {
   let n = Array.from({ length: 9 }, () => Math.floor(Math.random() * 10));
   let d1 = n.reduce((acc, num, i) => acc + num * (10 - i), 0) % 11;
@@ -59,23 +71,24 @@ function generateCPF() {
   d2 = d2 % 11 < 2 ? 0 : 11 - (d2 % 11);
   const cpf = `${n.join('')}${d1}${d2}`;
   const formattedCPF = isFormattingEnabled() ? formatCPF(cpf) : cpf;
-  copyToClipboard(formattedCPF);
+
+  if(isAutoCopyEnabled()) {
+    copyToClipboard(formattedCPF);
+  }
+
   setCookie("h_cpf", cpf, 365);
 
   return formattedCPF;
 }
 
-// Função para formatar CPF como 000.000.000-00
 function formatCPF(cpf) {
   return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
 }
 
-// Função para gerar CNPJ válido com opção de formatação e copiar para área de transferência
 function generateCNPJ() {
   let n = Array.from({ length: 8 }, () => Math.floor(Math.random() * 10)); // 8 dígitos base
   n.push(0, 0, 0, 1); // Completa com sufixo de empresa padrão (0001)
 
-  // Calcula o primeiro dígito verificador
   let d1 = calculateCNPJVerifierDigit(n);
   n.push(d1);
 
@@ -85,13 +98,16 @@ function generateCNPJ() {
 
   const cnpj = n.join('');
   const formattedCNPJ = isFormattingEnabled() ? formatCNPJ(cnpj) : cnpj;
-  copyToClipboard(formattedCNPJ);
+
+  if(isAutoCopyEnabled()) {
+    copyToClipboard(formattedCNPJ);
+  }
+
   setCookie("h_cnpj", cnpj, 365);
 
   return formattedCNPJ;
 }
 
-// Função auxiliar para calcular um dígito verificador de CNPJ
 function calculateCNPJVerifierDigit(numbers) {
   const multipliers = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
   const sum = numbers.reduce((acc, num, i) => acc + num * multipliers[i + (multipliers.length - numbers.length)], 0);
@@ -99,12 +115,10 @@ function calculateCNPJVerifierDigit(numbers) {
   return remainder < 2 ? 0 : 11 - remainder;
 }
 
-// Função para formatar CNPJ como 00.000.000/0000-00
 function formatCNPJ(cnpj) {
   return cnpj.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5");
 }
 
-// Função para gerar RG válido com opção de formatação e copiar para área de transferência
 function generateRG() {
   let rg = Array.from({ length: 8 }, () => Math.floor(Math.random() * 10)); // Gera os 8 primeiros dígitos
 
@@ -114,13 +128,15 @@ function generateRG() {
 
   const rgString = rg.join('');
   const formattedRG = isFormattingEnabled() ? formatRG(rgString) : rgString;
-  copyToClipboard(formattedRG);
-  setCookie("h_rg", rgString, 365);
 
+  if(isAutoCopyEnabled()) {
+    copyToClipboard(formattedRG);
+  }
+
+  setCookie("h_rg", rgString, 365);
   return formattedRG;
 }
 
-// Função auxiliar para calcular o dígito verificador do RG usando módulo 11
 function calculateRGVerifierDigit(numbers) {
   const sum = numbers.reduce((acc, num, i) => acc + num * (9 - i), 0);
   const remainder = sum % 11;
@@ -132,13 +148,10 @@ function calculateRGVerifierDigit(numbers) {
   }
 }
 
-// Função para formatar RG como 00.000.000-0
 function formatRG(rg) {
   return rg.replace(/(\d{2})(\d{3})(\d{3})([\dX])/, "$1.$2.$3-$4");
 }
 
-
-// Função para formatar Celular como (00) 00000-0000
 function formatCel(cel) {
   const lCelular = cel.replace(/(\d{2})(\d{5})(\d{4})/, "\($1\) $2-$3");
   return lCelular;
@@ -190,9 +203,12 @@ function generateCel() {
   let celNumber = ddd + "9" + celString;
 
   const numeroCelular = isFormattingEnabled() ? formatCel(celNumber) : celNumber;
-  copyToClipboard(numeroCelular);
-  setCookie("h_cel", celNumber, 365);
 
+  if(isAutoCopyEnabled()) {
+    copyToClipboard(numeroCelular);
+  }
+
+  setCookie("h_cel", celNumber, 365);
   return numeroCelular;
 }
 
@@ -219,8 +235,6 @@ function carregarHistorico(){
   }
 }
 
-
-// Configurações iniciais e evento de mudança no switch
 document.addEventListener('DOMContentLoaded', () => {
   const h_cpf = getCookie("h_cpf");
   const h_cnpj = getCookie("h_cnpj");
@@ -229,6 +243,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   loadFormatOption();
   document.getElementById("formatOption").addEventListener("change", saveFormatOption);
+
+  loadAutoCopyOption();
+  document.getElementById("autoCopyOption").addEventListener("change", saveAutoCopyOption);
 
   carregarHistorico();
 
@@ -247,4 +264,21 @@ document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('generateCel').addEventListener('click', () => {
     document.getElementById('celResult').value = generateCel();
   });
+
+  document.getElementById('cpfResult').addEventListener('click', () => {
+    document.getElementById('cpfResult').select();
+  });
+
+  document.getElementById('cnpjResult').addEventListener('click', () => {
+    document.getElementById('cnpjResult').select();
+  });
+
+  document.getElementById('rgResult').addEventListener('click', () => {
+    document.getElementById('rgResult').select();
+  });
+
+  document.getElementById('celResult').addEventListener('click', () => {
+    document.getElementById('celResult').select();
+  });
+
 });
